2eed0c444fd9abea9c385246f75fbdce89595768
mahmoud <ma7.aelaziz@gmail.com>
2019-10-21 16:56:34 +0400

    final commit

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,15 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     pub_date = models.DateTimeField()
+    
     post = models.TextField()
     image = models.ImageField(upload_to ='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.post[:100]
+    
+    def date(self):
+        return self.pub_date.strftime('%a %d %b %Y ')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Blog
 
 # Create your views here.
+def allblogs(request):
+    blog_post = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blog_post': blog_post})
+
+
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    
+    return render(request, 'blog/detail.html', {'blog':blog_detail})
